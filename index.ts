6eec2eafd7d848a7b58761a74a470b887307692c
Iganza-roy <iganzaroy55@gmail.com>
2025-09-15 17:07:47 +0300

    Implement text generation using ai-sdk and google model in index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import { generateText } from 'ai';
+// Import the google module from the ai-sdk package
+
+import { google } from '@ai-sdk/google';
+
+//specify the model to use for the generaating texting and prompt
+const { text } = await generateText({
+  model: google('models/gemini-2.5-flash'),
+  prompt: 'What is an AI agent?',
+});
+
+console.log(text);
